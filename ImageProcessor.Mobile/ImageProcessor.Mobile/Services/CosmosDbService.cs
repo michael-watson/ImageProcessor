d8125769287aada44566cac6fc75d48b559688bf
Michael Watson <miwats@michaelw-mac.local>
2017-10-18 19:15:23 -0700

    add simple mobile app

---
+++ ImageProcessor.Mobile/ImageProcessor.Mobile/Services/CosmosDbService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using Microsoft.Azure.Documents.Linq;
+using Microsoft.Azure.Documents.Client;
+
+namespace ImageProcessor.Mobile
+{
+	public class CosmosDbService
+	{
+		static readonly string DatabaseId = "metadata";
+		static readonly string CollectionId = "analyzed-images";
+		static readonly string CosmosUrl = "https://image-processing-miwats.documents.azure.com:443/";
+		static readonly string CosmosDbKey = "anlvPs5x0UuzHBptPgANpN6yKOnsLwcD3IwTcyvgNypBhCLsKNWueFgRyjEoXG3GvnXeZoY8ryJaWXrzsRZPxQ==";
+
+		static readonly DocumentClient documentClient = new DocumentClient(new Uri(CosmosUrl), CosmosDbKey);
+
+		public static async Task<List<ProcessedImage>> GetAllProcessedImagesAsync()
+		{
+			var items = new List<ProcessedImage>();
+
+			var query = documentClient.CreateDocumentQuery<ProcessedImage>(UriFactory.CreateDocumentCollectionUri(DatabaseId, CollectionId)).AsDocumentQuery();
+			while (query.HasMoreResults)
+			{
+				items.AddRange(await query.ExecuteNextAsync<ProcessedImage>());
+			}
+
+			return items;
+		}
+	}
+}
---
+++ ImageProcessor.Mobile/ImageProcessor.Mobile/ViewModels/ImageListPageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
+
+namespace ImageProcessor.Mobile
+{
+	public class ImageListPageViewModel : INotifyPropertyChanged
+	{
+		public ObservableCollection<ProcessedImage> Images { get; set; } = new ObservableCollection<ProcessedImage>();
+
+		public async Task GetAllImages()
+		{
+			var images = await CosmosDbService.GetAllProcessedImagesAsync();
+			foreach (var image in images)
+				Images.Add(image);
+		}
+
+		#region INotifyPropertyChanged Implementation
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+		#endregion
+	}
+}
